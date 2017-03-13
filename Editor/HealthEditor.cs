07d59bc7d74c101f53c75c4c2d4ba283fc397af9
Patryk <patrakus@interia.pl>
2017-03-13 16:13:17 +0100

    Added IntField
    Added IntField for setting damage for soldier.

--- Editor/HealthEditor.cs
+++ Editor/HealthEditor.cs
@@ -4,13 +4,17 @@ using UnityEditor;
 [CustomEditor(typeof(HealthComponent))]
 public class HealthEditor : Editor {
 
+    int damage = 100;
+
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
 
+        damage = EditorGUILayout.IntField(new GUIContent("Set damage:"), damage);
+
         if (GUILayout.Button("Hit"))
         {
-            ((HealthComponent)target).Healt -= 10;
+            ((HealthComponent)target).Healt -= damage;
         }
     }
 }
