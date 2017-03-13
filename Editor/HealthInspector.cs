f8c99f724d801ecdf5a5f044b82d1b2c1167711a
Patryk <patrakus@interia.pl>
2017-03-13 15:51:01 +0100

    Added script inspector
    Added script modifying inspector for HealtComponent

---
+++ Editor/HealthInspector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(HealthComponent))]
+public class HealthInspector : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Hit"))
+        {
+            ((HealthComponent)target).Healt -= 10;
+        }
+    }
+}
